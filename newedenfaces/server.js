5399f7cadd2d2caf451e090ebd6f78ab1e13f7f2
Todor Krastev <to6esko69@gmail.com>
2016-05-01 08:02:35 +0300

    fixed less problem

--- newedenfaces/server.js
+++ newedenfaces/server.js
@@ -39,6 +39,21 @@ app.use(function(req, res) {
 });
 
 
-app.listen(app.get('port'), function() {
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var onlineUsers = 0;
+
+io.sockets.on('connection', function(socket) {
+  onlineUsers++;
+
+  io.sockets.emit('onlineUsers', { onlineUsers: onlineUsers });
+
+  socket.on('disconnect', function() {
+    onlineUsers--;
+    io.sockets.emit('onlineUsers', { onlineUsers: onlineUsers });
+  });
+});
+
+server.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
